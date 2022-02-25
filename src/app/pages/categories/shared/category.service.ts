a8fd76f69b0aeff9fb6620296a3b0261e88fed5b
jeferson <jeferson@matriz.maxdata>
2022-02-25 17:51:51 -0300

    first commit

---
+++ src/app/pages/categories/shared/category.service.ts
@@ -0,0 +1,15 @@
+import { Injectable, Injector } from '@angular/core';
+import { Category } from "./category.model";
+
+import { BaseResourceService } from "../../../shared/services/base-resource.service";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoryService extends BaseResourceService<Category> {
+
+  constructor(protected injector: Injector) {
+    super("http://localhost:8080/category", injector, Category.fromJson);
+  }
+
+}
